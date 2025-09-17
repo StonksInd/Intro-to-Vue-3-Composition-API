9dd07e8e9dfecaf555c071000dbe3c52008b23f8
Nathanaël <129834668+StonksInd@users.noreply.github.com>
2025-09-17 10:00:44 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
 import { ref } from 'vue'
-
+import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 const product = ref('Socks')
-
+const image = ref(socksBlueImage)
+const url = ref( 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhNhtqtnswc6iC8kfqbAp70bKxbkRTogHLXJW56o0njSQBQDPsY4bkHEOQCivVd4-lamBr2rg9F9CnyYRlFpzaIpS69gkDGkGOvYA_b61w')
 </script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <img :src="image" alt="">
+        <img :src="url" alt="">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
